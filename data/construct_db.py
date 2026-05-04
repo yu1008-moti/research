590ac59207d4ba419304a76fa55781c1db61063a
yu1008-moti <264441008@ccmailg.meijo-u.ac.jp>
2026-05-04 13:50:29 +0900

    フォルダ構造をさらに少し変えた

--- data/construct_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-import os
-
-# equities_bars_daily.csv is exsits from 2009-05-07 to 2026-04-17
-#   homogenous graph -> node = [daily-price-info], edge = [price-correlation]
-# heterogenous graph -> node = [daily-price-info, quarterly-firm-info, yahoo-news, daily-index-info, other-quants-info], edge = [price-correlation]
-def create_db(db_name):
-    conn = sql.connect(db_name)
-    c = conn.cursor()
